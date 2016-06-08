518463c14245dc88b74858555c9a5715c6e7ebbf
Lance Bragstad <lbragstad@gmail.com>
2016-06-08 23:10:22 +0000

    Add more logging to benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -17,24 +17,28 @@ if ! grep -q 'Failed requests:        0' results/create_token; then
   echo 'WARNING: Failed requests!'
 fi
 git diff --color results/create_token | grep --fixed-strings ' [ms] (mean)' || true
+git diff --color results/create_token | grep --fixed-strings ' [#/sec] (mean)' || true
 
 echo "Benchmarking token validation..."
-ab -r -c 1 -n 10000 -T 'application/json' -H "X-Auth-Token: $ADMIN_TOKEN\n" -H "X-Subject-Token: $SUBJECT_TOKEN\n" http://$HOST:35357/v3/auth/tokens > results/validate_token
+ab -r -c 1 -n 1000 -T 'application/json' -H "X-Auth-Token: $ADMIN_TOKEN\n" -H "X-Subject-Token: $SUBJECT_TOKEN\n" http://$HOST:35357/v3/auth/tokens > results/validate_token
 if ! grep -q 'Failed requests:        0' results/validate_token; then
   echo 'WARNING: Failed requests!'
 fi
 git diff --color results/validate_token | grep --fixed-strings ' [ms] (mean)' || true
+git diff --color results/validate_token | grep --fixed-strings ' [#/sec] (mean)' || true
 
 echo "Benchmarking token creation concurrently..."
 ab -r -c 100 -n 2000 -p auth.json -T 'application/json' http://$HOST:35357/v3/auth/tokens > results/create_token_concurrent
 if ! grep -q 'Failed requests:        0' results/create_token_concurrent; then
   echo 'WARNING: Failed requests!'
 fi
+git diff --color results/create_token_concurrent | grep --fixed-strings ' [ms] (mean)' || true
 git diff --color results/create_token_concurrent | grep --fixed-strings ' [#/sec] (mean)' || true
 
 echo "Benchmarking token validation concurrency..."
-ab -r -c 100 -n 100000 -T 'application/json' -H "X-Auth-Token: $ADMIN_TOKEN" -H "X-Subject-Token: $SUBJECT_TOKEN" http://$HOST:35357/v3/auth/tokens > results/validate_token_concurrent
+ab -r -c 100 -n 10000 -T 'application/json' -H "X-Auth-Token: $ADMIN_TOKEN" -H "X-Subject-Token: $SUBJECT_TOKEN" http://$HOST:35357/v3/auth/tokens > results/validate_token_concurrent
 if ! grep -q 'Failed requests:        0' results/validate_token_concurrent; then
   echo 'WARNING: Failed requests!'
 fi
+git diff --color results/validate_token_concurrent | grep --fixed-strings ' [ms] (mean)' || true
 git diff --color results/validate_token_concurrent | grep --fixed-strings ' [#/sec] (mean)' || true

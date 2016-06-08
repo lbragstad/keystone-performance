19039921466b9eb4647f82727c4b9a094adc1c89
Lance Bragstad <lbragstad@gmail.com>
2016-06-08 21:33:31 +0000

    Fix run_everything.sh

--- run_everything.sh
+++ run_everything.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-curl https://bootstrap.pypa.io/get-pip.py | sudo python
-sudo apt-get update
-sudo apt-get install -y build-essential python-dev libffi-dev libssl-dev
-sudo mkdir /etc/ansible
-sudo pip install -r requirements.txt
+set -e
+
+curl https://bootstrap.pypa.io/get-pip.py | python
+apt-get update
+apt-get install -y build-essential python-dev libffi-dev libssl-dev
+mkdir /etc/ansible
+pip install -r requirements.txt
 ansible-galaxy install --role-file=ansible-role-requirements.yml --force
 ansible-playbook -i inventory_localhost --sudo setup_database.yml
 ansible-playbook -i inventory_localhost --sudo setup_perf_host.yml

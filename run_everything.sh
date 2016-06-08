d9e7fc75f0873a9c3d6c9868af5613914e1324c7
Lance Bragstad <lbragstad@gmail.com>
2016-06-08 21:57:36 +0000

    more fixes

--- run_everything.sh
+++ run_everything.sh
@@ -4,8 +4,10 @@ set -e
 
 apt-get update
 apt-get install -y build-essential python-dev libffi-dev libssl-dev
-curl /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py | python3
-mkdir /etc/ansible
+curl /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py | python
+if [ ! -d /etc/ansible ]; then
+  mkdir /etc/ansible
+fi
 pip install -r requirements.txt
 ansible-galaxy install --role-file=ansible-role-requirements.yml --force
 ansible-playbook -i inventory_localhost --sudo setup_database.yml

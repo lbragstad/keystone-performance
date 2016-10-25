a972a17ae77ff554d8ff491695411b330ca641e7
Lance Bragstad <lbragstad@gmail.com>
2016-10-25 17:55:41 +0000

    Make benchmark.sh report all results

--- benchmark.sh
+++ benchmark.sh
@@ -12,17 +12,7 @@ echo "Warming up Apache..."
 ab -c 100 -n 1000 -T 'application/json' http://$HOST:35357/ > /dev/null 2>&1
 
 echo "Benchmarking token creation..."
-ab -r -c 1 -n 200 -p auth.json -T 'application/json' http://$HOST:35357/v3/auth/tokens > results/create_token
-if ! grep -q 'Failed requests:        0' results/create_token; then
-  echo 'WARNING: Failed requests!'
-fi
-git diff --color results/create_token | grep --fixed-strings ' [ms] (mean)' || true
-git diff --color results/create_token | grep --fixed-strings ' [#/sec] (mean)' || true
+ab -r -c 1 -n 200 -p auth.json -T 'application/json' http://$HOST:35357/v3/auth/tokens
 
 echo "Benchmarking token validation..."
-ab -r -c 1 -n 1000 -T 'application/json' -H "X-Auth-Token: $ADMIN_TOKEN\n" -H "X-Subject-Token: $SUBJECT_TOKEN\n" http://$HOST:35357/v3/auth/tokens > results/validate_token
-if ! grep -q 'Failed requests:        0' results/validate_token; then
-  echo 'WARNING: Failed requests!'
-fi
-git diff --color results/validate_token | grep --fixed-strings ' [ms] (mean)' || true
-git diff --color results/validate_token | grep --fixed-strings ' [#/sec] (mean)' || true
+ab -r -c 1 -n 1000 -T 'application/json' -H "X-Auth-Token: $ADMIN_TOKEN" -H "X-Subject-Token: $SUBJECT_TOKEN" http://$HOST:35357/v3/auth/tokens
